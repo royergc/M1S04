fb9754541bf3e24b89f061703292225f5943d2fc
Guilherme Castro Royer <guilherme.royer@gmail.com>
2021-11-27 20:41:01 -0300

    Implementado exercício 3 - Calculadora IMC

---
+++ src/Ex03CalculadoraIMC.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Ex03CalculadoraIMC {
+    public static void main(String[] args) {
+        double altura, peso, imc;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite sua altura em metros com 2 casas decimais (Ex: 1,82): ");
+        altura = scanner.nextDouble();
+
+        System.out.println("Por favor, digite seu peso em kg: ");
+        peso = scanner.nextDouble();
+
+        imc = peso / (altura * altura);
+
+        System.out.printf("Seu IMC é: %.2f%n", imc);
+    }
+}
