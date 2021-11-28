5154d5afce2d8ee5c21c46d11d3c6743be41682c
Guilherme Castro Royer <guilherme.royer@gmail.com>
2021-11-28 01:40:42 -0300

    Modificada a geracao do numero randomico de Math.random() para Random.nextInt() por sugestao do Lint e adicionada verificacao para evitar que o usuario insira numero que nao esteja entre 1 e 5

--- src/com/m1s04/techdive/Ex05AdivinhaNumero.java
+++ src/com/m1s04/techdive/Ex05AdivinhaNumero.java
@@ -1,17 +1,22 @@
 package com.m1s04.techdive;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Ex05AdivinhaNumero {
     public static void main(String[] args) {
-        int numeroSorteado;
-        int numeroEscolhido;
 
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
 
         System.out.println("Adivinhe qual número de 1 a 5 eu estou pensando.");
-        numeroEscolhido = scanner.nextInt();
-        numeroSorteado = (int) ((Math.random() * 5) + 1);
+        int numeroEscolhido = scanner.nextInt();
+        while(numeroEscolhido < 1 || numeroEscolhido > 5) {
+            System.out.println("Numero escolhido precisa estar entre 1 e 5, escolha novamente");
+            numeroEscolhido = scanner.nextInt();
+        }
+//        numeroSorteado = (int) ((Math.random() * 5) + 1);
+        int numeroSorteado = random.nextInt(5) + 1;
 
         if(numeroEscolhido == numeroSorteado) {
             System.out.println("Voce acertou!");
